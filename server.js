7ac0b18da64cf2e2b5f64fa8dc2da2e8ab64b3e1
tw.eqsl-server <alpine67@live.fr>
2025-11-27 10:17:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post("/upload", async (req, res) => {
             <text x="20" y="100" font-size="28">Date : ${req.body.date}</text>
             <text x="20" y="140" font-size="28">UTC : ${req.body.time}</text>
             <text x="20" y="180" font-size="28">Bande : ${req.body.band}</text>
-            <text x="20" y="200" font-size="28">Mode : ${req.body.mode}</text>
+            <text x="20" y="220" font-size="28">Mode : ${req.body.mode}</text>
             <text x="20" y="260" font-size="28">Report : ${req.body.report}</text>
 
             <text x="20" y="280" font-size="24">${noteText}</text>
